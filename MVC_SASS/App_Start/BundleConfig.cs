f4aa84ce35679e3a1a17e5bbe4f5f86458a72393
Frank <franklanglois@gmail.com>
2017-12-12 21:24:17 -0500

    Added Sass files.

--- MVC_SASS/App_Start/BundleConfig.cs
+++ MVC_SASS/App_Start/BundleConfig.cs
@@ -25,7 +25,8 @@ namespace MVC_SASS
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/SASS/MyStyles.css"));
         }
     }
 }
