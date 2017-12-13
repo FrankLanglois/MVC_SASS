b981a442886e22ab3e6b95fe469abef3c4c099b3
Frank <franklanglois@gmail.com>
2017-12-12 20:32:50 -0500

    Add project files.

---
+++ MVC_SASS/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC_SASS.MvcApplication" Language="C#" %>
---
+++ MVC_SASS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_SASS.Startup))]
+namespace MVC_SASS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
